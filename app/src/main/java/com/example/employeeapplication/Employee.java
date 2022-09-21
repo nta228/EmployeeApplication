c155ff784b56b40dddbd5dc6aae4752ed65361a8
nta228 <64982003+nta228@users.noreply.github.com>
2022-09-22 01:37:38 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/employeeapplication/Employee.java
@@ -0,0 +1,56 @@
+package com.example.practicalapplication;
+
+public class Employee {
+    private long id;
+    private String name;
+    private String designation;
+    private double salary;
+
+    public Employee() {
+    }
+
+    public Employee(long id, String name, String designation, double salary) {
+        this.id = id;
+        this.name = name;
+        this.designation = designation;
+        this.salary = salary;
+    }
+
+    public Employee(String name, String designation, double salary) {
+        this.name = name;
+        this.designation = designation;
+        this.salary = salary;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
--- app/src/main/java/com/example/employeeapplication/MainActivity.java
+++ app/src/main/java/com/example/employeeapplication/MainActivity.java
@@ -1,66 +1,144 @@
-package com.example.employeeapplication;
-
-import android.app.Activity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.PopupWindow;
-import android.widget.Toast;
+package com.example.practicalapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    ArrayList<Employee> employees;
-    com.example.employeeapplication.SqlLiteDatabaseHandler db;
-    Button save;
-    PopupWindow pwindo;
-    Activity activity;
-    ListView listView;
-    CustomEmployeeList customEmployeeList;
+    private Button btnSave, btnUpdate, btnDelete, btnCancel;
+    private EmployeeAdapter employeeAdapter;
+    private ListView lvEmployee;
+    private List<Employee> employees;
+    private SQLiteHelper sqLiteHelper;
+    private EditText editName, editDesignation, editSalary;
+    private Employee employeeSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSave = findViewById(R.id.btnSave);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnDelete = findViewById(R.id.btnDelete);
+        btnCancel = findViewById(R.id.btnCancel);
+        lvEmployee = findViewById(R.id.lvEmployee);
+        editName = findViewById(R.id.editName);
+        editDesignation = findViewById(R.id.editDesignation);
+        editSalary = findViewById(R.id.editSalary);
+        sqLiteHelper = new SQLiteHelper(this);
+        employees = sqLiteHelper.findAllEmployee();
+        employeeAdapter = new EmployeeAdapter(this, employees, sqLiteHelper);
+        lvEmployee.setAdapter(employeeAdapter);
 
-        activity=this;
-        db= new com.example.employeeapplication.SqlLiteDatabaseHandler(this);
-        listView = (ListView) findViewById(R.id.lsContact);
-        save = (Button) findViewById(R.id.save);
-        save.setOnClickListener(new View.OnClickListener() {
+        btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                if(!isValidForm()) {
+                    Toast.makeText(MainActivity.this, "Invalid form, please try again !",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    String name = editName.getText().toString();
+                    String designation = editDesignation.getText().toString();
+                    Double salary = Double.parseDouble(editSalary.getText().toString());
+                    Employee employee = new Employee(name, designation, salary);
+                    sqLiteHelper.createEmployee(employee);
+                    employees.add(employee);
+                    employeeAdapter.notifyDataSetChanged();
+                    resetForm();
+                }
             }
         });
-        Log.d("MainActivity: ", "Before reading mainactivity");
-        employees = (ArrayList) db.getAllEmployees();
 
-        for (Employee employee : employees) {
-            String log = "Id: " + employee.getId() + " ,Name: " + employee.getEmployeeName() + " ,designation: " + employee.getDesignation()+ " ,Salary: " + employee.getSalary();
-            // Writing Countries to log
-            Log.d("Name: ", log);
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!isValidForm()) {
+                    Toast.makeText(MainActivity.this, "Invalid form, please try again !",
+                            Toast.LENGTH_SHORT).show();
+                }else {
+                    String name = editName.getText().toString();
+                    String designation = editDesignation.getText().toString();
+                    Double salary = Double.parseDouble(editSalary.getText().toString());
+                    Employee employee = new Employee(employeeSelected.getId(), name, designation, salary);
+                    sqLiteHelper.updateEmployee(employee);
+                    employees = sqLiteHelper.findAllEmployee();
+                    employeeAdapter.updateData(employees);
+                    cancelForm();
+                }
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sqLiteHelper.deleteEmployee(employeeSelected);
+                employees.remove(employeeSelected);
+                employeeAdapter.notifyDataSetChanged();
+                cancelForm();
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelForm();
+            }
+        });
+
+        lvEmployee.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Employee employee = employees.get(position);
+                editName.setText(employee.getName());
+                editDesignation.setText(employee.getDesignation());
+                editSalary.setText(String.valueOf(employee.getSalary()));
+                employee.setId(id);
+                employeeSelected = employee;
+                btnUpdate.setVisibility(View.VISIBLE);
+                btnCancel.setVisibility(View.VISIBLE);
+                btnDelete.setVisibility(View.VISIBLE);
+                btnSave.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    public boolean isValidForm() {
+        String name = editName.getText().toString();
+        String designation = editDesignation.getText().toString();
+        Double salary = editSalary.getText().toString().length() > 0 ? Double.parseDouble(editSalary.getText().toString()) : 0;
+        boolean isValid = true;
+        if(name.length() == 0) {
+            isValid = false;
         }
+        if(designation.length() == 0) {
+            isValid = false;
+        }
+        if(salary == 0) {
+            isValid = false;
+        }
+        return isValid;
+    }
 
-        CustomEmployeeList customEmployeeList = new CustomEmployeeList(this, employees, db);
-        listView.setAdapter(customEmployeeList);
+    public void resetForm() {
+        editName.setText("");
+        editDesignation.setText("");
+        editSalary.setText("");
+    }
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                Toast.makeText(getApplicationContext(), "You Selected " + employees.get(position).getEmployeeName() + " as Employee", Toast.LENGTH_SHORT).show();
-            }
-        });
+    public void cancelForm() {
+        resetForm();
+        btnUpdate.setVisibility(View.GONE);
+        btnCancel.setVisibility(View.GONE);
+        btnDelete.setVisibility(View.GONE);
+        btnSave.setVisibility(View.VISIBLE);
     }
 }
